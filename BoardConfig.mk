f1e158d2aa15fb8b1c7c4cc3b98b276d03e78706
Steve Arnold <nerdboy@gentoo.org>
2017-10-19 14:05:05 -0700

    Revert kernel args to ttyMSM0 instead of ttyHSL0 for kgdb on 3.10
    Signed-off-by: Steve Arnold <nerdboy@gentoo.org>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,7 +78,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 
-BOARD_KERNEL_CMDLINE := initcall_debug=1 androidboot.hardware=qcom kgdboc=ttyHSL0,115200 kgdbwait console=ttyHLS0,115200n8 user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := initcall_debug=1 androidboot.hardware=qcom kgdboc=ttyMSM0,115200 kgdbwait console=ttyMSM0,115200n8 user_debug=31 msm_rtb.filter=0x3b7 ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_SEPARATED_DT := true
 
 BOARD_CUSTOM_BOOTIMG_MK := device/fairphone/FP2/mkbootimg-test.mk
